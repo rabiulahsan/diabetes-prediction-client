460028305730200acab355fbedc270948938e606
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-03 03:27:05 +0600

    add titlebar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-center items-center py-4 bg-gray-100">
       <p className="font-bold text-slate-500 text-3xl italic cursor-pointer">
-        Predict <span className="text-green-500 ">Diabetes</span>
+        Predict<span className="text-green-500 ">Diabetes</span>
       </p>
     </div>
   );
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./Components/Navbar";
+import TitleBar from "./Components/TitleBar";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <TitleBar></TitleBar>
     </div>
   );
 };
